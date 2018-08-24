4467c2f6f0f6499c911c38998e3ea4c909ed04c7
Shaofeng Jiang <sf.jiang.ca@gmail.com>
2018-08-24 13:19:00 -0400

    update proto

--- src/main/java/com/ele/model/mysql/Shop.java
+++ src/main/java/com/ele/model/mysql/Shop.java
@@ -5,41 +5,32 @@ import com.ele.model.EleImmutable;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.immutables.value.Value;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 @SuppressWarnings("javadoc")
 @EleImmutable
-public interface ShopProfile extends DataModel {
+public interface Shop extends DataModel {
     default UUID id() {
         return UUID.randomUUID();
     }
-    String ImgUrl();
-
-    boolean isBrand();
-
     String shopName();
-
-    double starNum();
-
-    int monthlySales();
-
-    double initMoney();
-
-    double deliveryFee();
-
-    double distance();
-
-    String needTime();
-
+    String shopAddress();
+    String shopURL();
+    String shopService();
+    String shopAnnouncement();
+    String shopSlogan();
+    float shopDeliveryFee();
+    float shopSendThreshold();
+    Date shopRegistration();
+    float shopLat();
+    float shopLng();
     boolean isBird();
-
-    boolean isOntime();
-
     boolean isInsurance();
-
-    boolean needtip();
-
-    boolean isNewShop();
+    boolean isBrand();
+    boolean needTip();
+    int activityNum();
+    String promotion();
 
 }
